d5068d1109419ee2fc1b65db00a89de40e506e1d
R.BALA SURYA <146628110+balasurya03@users.noreply.github.com>
2026-04-14 23:42:04 -0700

    schemas.py

--- app/schemas.py
+++ app/schemas.py
@@ -2,19 +2,19 @@ from pydantic import BaseModel
 from datetime import datetime
 
 
-# 🔹 Base schema (shared fields)
+# Base schema (shared fields)
 class TimeLogBase(BaseModel):
     task: str
     project: str
     time_spent: str
 
 
-# 🔹 Create schema (input)
+# Create schema (input)
 class TimeLogCreate(TimeLogBase):
     pass
 
 
-# 🔹 Response schema (output)
+# Response schema (output)
 class TimeLogResponse(TimeLogBase):
     id: int
     created_at: datetime   # 🔥 added (important)
